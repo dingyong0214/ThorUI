f850dc5ef1937fab4495c2d0958ae4df8764a1bc
dingyong <1062884167@qq.com>
2019-06-28 10:17:42 +0800

    商城模板完善
    新增商城列表，详情

---
+++ components/numberbox/numberbox.js
@@ -0,0 +1,119 @@
+Component({
+  externalClasses: ['tui-numberbox-class'],
+  properties: {
+    value: {
+      type: Number,
+      value: 1
+    },
+    //最小值
+    min: {
+      type: Number,
+      value: -Infinity
+    },
+    //最大值
+    max: {
+      type: Number,
+      value: Infinity
+    },
+    //迈步大小 1 1.1 10...
+    step: {
+      type: Number,
+      value: 1
+    },
+    //是否禁用操作
+    disabled: {
+      type: Boolean,
+      value: false
+    },
+    //加减图标大小 rpx
+    iconsize: {
+      type: Number,
+      value: 24
+    },
+    iconcolor: {
+      type: String,
+      value: "#333"
+    },
+    //input 高度
+    height: {
+      type: Number,
+      value: 50
+    },
+    //input 宽度
+    width: {
+      type: Number,
+      value: 90
+    },
+    //input 背景颜色
+    bgcolor: {
+      type: String,
+      value: "#f2f2f2"
+    },
+    //input 字体颜色
+    color: {
+      type: String,
+      value: "#333"
+    }
+  },
+  data: {
+
+  },
+  methods: {
+    getScale() {
+      let scale = 1;
+      //浮点型
+      if (this.data.step != parseInt(this.data.step)) {
+        scale = Math.pow(10, (this.data.step + '').split('.')[1].length)
+      }
+      return scale;
+    },
+    calcNum: function(type) {
+      if (this.data.disabled) {
+        return
+      }
+      const scale = this.getScale()
+      let num = this.data.value * scale
+      let step = this.data.step * scale
+      if (type === 'reduce') {
+        num -= step
+      } else if (type === 'plus') {
+        num += step
+      }
+      let value = num / scale
+      if (value < this.data.min || value > this.data.max) {
+        return
+      }
+
+      this.handleChange(value, type)
+    },
+    plus: function() {
+      this.calcNum("plus")
+    },
+    reduce: function() {
+      this.calcNum("reduce")
+    },
+    blur: function(e) {
+      let value = e.detail.value
+      if (value) {
+        value = +value
+        if (value > this.data.max) {
+          value = this.data.max
+        } else if (value < this.data.min) {
+          value = this.data.min
+        }
+      } else {
+        value = this.data.min
+      }
+      this.handleChange(value, "blur")
+    },
+    handleChange(value, type) {
+      if (this.data.disabled) {
+        return
+      }
+      this.triggerEvent('change', {
+        value: value,
+        type: type
+      })
+    }
+  }
+})
--- pages/extend-view/productDetail/productDetail.js
+++ pages/extend-view/productDetail/productDetail.js
@@ -1,66 +1,137 @@
-// pages/extend-view/productDetail/productDetail.js
+const util = require('../../../utils/util.js')
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    height: 64, //header高度
+    top: 0, //标题图标距离顶部距离
+    scrollH: 0, //滚动总高度
+    opcity: 0,
+    iconOpcity: 0.5,
+    banner: [
+      "https://www.thorui.cn/img/product/11.jpg",
+      "https://www.thorui.cn/img/product/2.png",
+      "https://www.thorui.cn/img/product/33.jpg",
+      "https://www.thorui.cn/img/product/4.png",
+      "https://www.thorui.cn/img/product/55.jpg",
+      "https://www.thorui.cn/img/product/6.png",
+      "https://www.thorui.cn/img/product/7.jpg",
+      "https://www.thorui.cn/img/product/8.jpg"
+    ],
+    bannerIndex: 0,
+    topMenu: [{
+      icon: "message",
+      text: "消息",
+      size: 26,
+      badge: 3
+    }, {
+      icon: "home",
+      text: "首页",
+      size: 23,
+      badge: 0
+    }, {
+      icon: "people",
+      text: "我的",
+      size: 26,
+      badge: 0
+    }, {
+      icon: "cart",
+      text: "购物车",
+      size: 23,
+      badge: 2
+    }, {
+      icon: "kefu",
+      text: "客服小蜜",
+      size: 26,
+      badge: 0
+    }, {
+      icon: "feedback",
+      text: "我要反馈",
+      size: 23,
+      badge: 0
+    }, {
+      icon: "share",
+      text: "分享",
+      size: 26,
+      badge: 0
+    }],
+    menuShow: false,
+    popupShow: false,
+    value: 1,
+    collected: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    let obj = wx.getMenuButtonBoundingClientRect();
+    this.setData({
+      width: obj.left,
+      height: obj.top + obj.height + 8,
+      top: obj.top + (obj.height - 32) / 2
+    }, () => {
+      wx.getSystemInfo({
+        success: (res) => {
+          this.setData({
+            scrollH: res.windowWidth
+          })
+        }
+      })
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  bannerChange: function (e) {
+    this.setData({
+      bannerIndex: e.detail.current
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  previewImage: function (e) {
+    let index = e.currentTarget.dataset.index;
+    wx.previewImage({
+      current: this.data.banner[index],
+      urls: this.data.banner
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //页面滚动执行方式
+  onPageScroll(e) {
+    let scroll = e.scrollTop <= 0 ? 0 : e.scrollTop;
+    let opcity = scroll / this.data.scrollH;
+    if (this.data.opcity >= 1 && opcity >= 1) {
+      return;
+    }
+    this.setData({
+      opcity: opcity,
+      iconOpcity: 0.5 * (1 - opcity < 0 ? 0 : 1 - opcity)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  back: function () {
+    wx.navigateBack()
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  openMenu: function () {
+    this.setData({
+      menuShow: true
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  closeMenu: function () {
+    this.setData({
+      menuShow: false
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  showPopup: function () {
+    this.setData({
+      popupShow: true
+    })
+  },
+  hidePopup: function () {
+    this.setData({
+      popupShow: false
+    })
+  },
+  change: function (e) {
+    this.setData({
+      value: e.detail.value
+    })
+  },
+  collecting: function () {
+    this.setData({
+      collected: !this.data.collected
+    })
+  },
+  common: function () {
+    util.toast("功能开发中~")
   }
 })
