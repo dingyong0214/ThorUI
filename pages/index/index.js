162953337dd87f84f4c274f2f8286398dd1819c1
echo0214 <1062884167@qq.com>
2021-10-01 18:53:51 +0800

    V1.7.1更新

--- pages/index/index.js
+++ pages/index/index.js
@@ -43,25 +43,6 @@ Page({
         }
       }).catch((res) => {})
     }, 50)
-    // 在页面中定义插屏广告
-    let interstitialAd = null
-
-    // 在页面onLoad回调事件中创建插屏广告实例
-    if (wx.createInterstitialAd) {
-      interstitialAd = wx.createInterstitialAd({
-        adUnitId: 'adunit-44bbe9a9087910e3'
-      })
-      interstitialAd.onLoad(() => {})
-      interstitialAd.onError((err) => {})
-      interstitialAd.onClose(() => {})
-    }
-
-    // 在适合的场景显示插屏广告
-    if (interstitialAd) {
-      interstitialAd.show().catch((err) => {
-        console.error(err)
-      })
-    }
   },
   data: {
     list: [{
